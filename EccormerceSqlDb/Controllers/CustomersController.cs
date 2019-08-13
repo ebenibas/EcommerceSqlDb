46f21e6f1ea11c1a16c8e0c6e706d17e57b28019
Ebenezer Obaseki <ebenibas@outlook.com>
2019-08-13 14:29:40 -0400

    Add project files.

---
+++ EccormerceSqlDb/Controllers/CustomersController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EccormerceSqlDb;
+using EccormerceSqlDb.Models;
+
+namespace EccormerceSqlDb.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private EcommerceDbEntities db = new EcommerceDbEntities();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(db.Customers.ToList());
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,BirthDate,AddressCountry")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,BirthDate,AddressCountry")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public ActionResult CustomersByCity()
+        {
+            var AllCities = db.Customers.Select(c => c.AddressCountry).Distinct().ToList();
+            ViewBag.City =new SelectList (AllCities);
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult CustomersByCity(string city)
+        {
+            CustomerManager manager = new CustomerManager();
+            var AllCities = db.Customers.Select(c => c.AddressCountry).Distinct().ToList();
+            ViewBag.City = new SelectList(AllCities);
+            var customersInThisCity = manager.GetCustomersInCity(city);
+            return View(customersInThisCity);
+
+        }
+    }
+}
---
+++ EccormerceSqlDb/Models/CustomerManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EccormerceSqlDb.Models
+{
+    public class CustomerManager
+    {
+        EcommerceDbEntities db = new EcommerceDbEntities();
+
+        public IList<Customer>GetCustomersInCity(string city)
+        {
+            var result = db.Customers.Where(c => c.AddressCountry==city).ToList();
+            return result;
+        }
+    }
+}
